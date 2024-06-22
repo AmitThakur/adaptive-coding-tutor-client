8b39513d0cedaa6c3e4a6cca0c75a40e00b19e33
Amit Thakur <hello@amitthakur.org>
2024-06-22 14:51:35 -0700

    Added some sample code

--- src/app/code-editor/code-editor.component.ts
+++ src/app/code-editor/code-editor.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-code-editor',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule ],
   templateUrl: './code-editor.component.html',
   styleUrl: './code-editor.component.css'
 })
 export class CodeEditorComponent {
+  code = `# This program adds two numbers
 
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))`;
+
+  public onValueChange(value: any): void {
+    console.log('Value changed...');
+    console.log(value);
+  }
 }
